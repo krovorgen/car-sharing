f306e93f0bb80c50eb814bbc23a710fe399241dd
Максим Сергеевич <krovorgen@gmail.com>
2021-02-12 23:56:03 +0300

    Complete desctop version

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+// prettier-ignore
+/* eslint-disable */
+$(function () {
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+    })
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+    })
+})
+/* eslint-enable */
