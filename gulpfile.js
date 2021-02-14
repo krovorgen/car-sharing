4dd77dfa4080e0cf6aacc812ac4271f00c96e03b
Максим Сергеевич <krovorgen@gmail.com>
2021-02-14 13:03:35 +0300

    Complete optimization

--- gulpfile.js
+++ gulpfile.js
@@ -25,9 +25,6 @@ function images() {
     return src('app/images/**/*')
         .pipe(
             imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 75, progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
                 imagemin.svgo({
                     plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
                 }),
